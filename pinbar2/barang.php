2b63287154ed10aa320e4c19e5fee3ccb8c30ea9
Almeer <almer433@gmail.com>
2021-11-27 22:38:41 +0700

    Melanjutkan sistem informasi peminjaman barang TIK

---
+++ pinbar2/barang.php
@@ -0,0 +1,197 @@
+<?php
+require "proses/session.php";
+$select = mysqli_query($conn, "SELECT * FROM tb_barang");
+
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Sistem Informasi Peminjaman Barang Jurusan TIK</title>
+</head>
+
+<body>
+    <div class="container-fluid">
+        <!-- Navbar Header -->
+        <?php
+        require "header.php";
+        ?>
+        <!-- akhir login -->
+    </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-3">
+                <?php
+                require "sidebar.php";
+                ?>
+            </div>
+            <!-- isi konten -->
+            <div class="col-9 ">
+                <div class="card ms-1 mt-4">
+                    <h3 class="card-header">
+                        <svg class="bi me-2" width="28" height="26">
+                                <use xlink:href="#grid" />
+                            </svg>Peminjaman Barang</h3>
+                    
+                </div>
+                <hr>
+                <table class="table table-striped  table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col">No</th>
+                            <th scope="col">Nama Barang</th>
+                            <th scope="col">Keterangan</th>
+                            <th scope="col">Gambar</th>
+                            <th scope="col">Aksi</th>
+                            <!-- isi table -->
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php $i = 1; ?>
+                        <?php foreach ($select as $sl) : ?>
+                            <tr>
+                                <th scope="row"><?= $i ?></th>
+                                <td><?= $sl['nama_barang']; ?></td>
+                                <td><?= $sl['keterangan']; ?></td>
+                                <td><?= $sl['gambar']; ?></td>
+                                <?php
+                                if ($row['level'] == 'admin') {
+                                ?>
+                                    <td>
+                                        <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                                                <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
+                                            </svg></button></th>
+                                        <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#exampleModal"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                                <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                                                <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+                                            </svg></button>
+                                    </td>
+                                <?php
+                                }
+                                if ($row['level'] == 'mahasiswa') {
+                                ?>
+                                    <td>
+                                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Pinjam</button>
+                                    </td>
+                                <?php
+                                }
+                                ?>
+                            </tr>
+                            <?php $i++ ?>
+                        <?php endforeach; ?>
+                        <!-- akhir isi table -->
+                    </tbody>
+                </table>
+                <!-- <div class="row">
+                    <div class="card ms-1 mt-4" style="width: 18rem;">
+                        <img src="https://dealharga.com/wp-content/uploads/2018/03/BenQ-MX611-a.jpg" class="card-img-top" alt="center">
+                        <div class="card-body">
+                            <h5 class="card-title">Proyektor<h5>
+                                    <p class="card-text"></p>
+                                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">Pinjam</button>
+                        </div>
+                    </div>
+
+                    <div class="card ms-1 mt-4" style="width: 18rem;">
+                        <img src="https://images.tokopedia.net/img/cache/500-square/product-1/2019/2/22/484880/484880_1b39c91d-b18a-4e1c-b9dd-61123124ca45_700_700.jpg" class="card-img-top" alt="rigth">
+                        <div class="card-body">
+                            <h5 class="card-title">Stop Kontak</h5>
+                            <p class="card-text"></p>
+                            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">Pinjam</button>
+                        </div>
+                    </div>
+                </div>
+            </div> -->
+
+                <!-- MODAL ADMIN -->
+                <?php
+                if ($row['level'] == 'admin') {
+                ?>
+                    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">Ubah data dibawah ini</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <form>
+                                        <div class="mb-3">
+                                            <label for="recipient-name" class="col-form-label">Nama Barang:</label>
+                                            <input type="text" class="form-control" id="recipient-name">
+                                        </div>
+                                        <div class="mb-3">
+                                            <label for="recipient-text" class="col-form-label">Keterangan:</label>
+                                            <input type="text" class="form-control" id="recipient-name">
+                                        </div>
+                                        <div class="mb-3">
+                                            <label for="recipient-text" class="col-form-label">Gambar:</label>
+                                            <textarea class="form-control" id="message-text"></textarea>
+                                    </form>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    <button type="button" class="btn btn-primary">Ubah</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- MODAL HAPUS -->
+                  
+                    <!-- MODAL MAHASISWA -->
+                <?php
+                }
+                if ($row['level'] == 'mahasiswa') {
+                ?>
+                    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">Isi data dibawah ini</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <form>
+                                        <div class="mb-3">
+                                            <label for="recipient-name" class="col-form-label">Username:</label>
+                                            <input type="text" class="form-control" id="recipient-name">
+                                        </div>
+                                        <div class="mb-3">
+                                            <label for="recipient-text" class="col-form-label">Barang:</label>
+                                            <input type="text" class="form-control" id="recipient-name">
+                                        </div>
+                                        <div class="mb-3">
+                                            <label for="recipient-text" class="col-form-label">Kelas:</label>
+                                            <textarea class="form-control" id="message-text"></textarea>
+                                    </form>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    <button type="button" class="btn btn-primary">Pinjam</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                <?php
+                }
+                ?>
+                <!-- MODAL MAHASISWA TUTUP -->
+            </div>
+        </div>
+    </div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script> -->
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+</body>
+
+</html>
